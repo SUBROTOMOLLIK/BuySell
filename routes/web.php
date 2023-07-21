9f3141188a50c391003ad00ea58a176cf8d5321b
SUBROTOMOLLIK <subrotomollik66@gmail.com>
2023-07-21 23:11:18 +0600

    change auth namespace

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
